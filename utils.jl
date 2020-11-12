d2b161d3f9db7f60f097fd98f7a230b0ba8a2838
Shuhei Kadowaki <aviatesk@gmail.com>
2020-11-12 21:04:08 +0900

    smarter weave cell cache

--- utils.jl
+++ utils.jl
@@ -166,14 +166,13 @@ A simple command to render/evaluate code in Weave.jl-like way.
 function lx_weave(com, _)
     content = Franklin.content(com.braces[1])
     lines = split(content, '\n')
-    Core.eval(Main, :(lines = $(lines)))
 
     i = findfirst(startswith("```julia"), lines)
     @assert !isnothing(i) "couldn't find Weave.jl header"
     lines = lines[i:end]
 
     header = first(lines)
-    id = string("weave-cell-id-", hash(gensym()))
+    id = string("weave-cell-id-", hash(join(lines))) # give same id for same code
     lines[1] = string(header, ':', id)
 
     push!(lines, "\\show{$(id)}")
